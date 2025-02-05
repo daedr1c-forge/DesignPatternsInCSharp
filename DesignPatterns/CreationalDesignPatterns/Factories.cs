c02a5732f73868658e17ed9c3b30de3e2cd87330
Innovate Core <kolaymaksimiv@gmail.com>
2025-02-05 20:22:39 +0200

    Object Tracking and Bulk Replacement

--- DesignPatterns/CreationalDesignPatterns/Factories.cs
+++ DesignPatterns/CreationalDesignPatterns/Factories.cs
@@ -1,4 +1,6 @@
-﻿namespace DesignPatterns.CreationalDesignPatterns;
+﻿using System.Text;
+
+namespace DesignPatterns.CreationalDesignPatterns;
 
 /// <summary>
 /// Factories
@@ -93,6 +95,99 @@ public static class Factories
         }
     }
 
+    //################################################################################################################
+    //#######################################  Object Tracking and Bulk Replacement  #################################
+    //################################################################################################################
+
+
+    public interface ITheme
+    {
+        string TextColor { get; }
+        string BgrColor { get; }
+    }
+
+    public class LightTheme : ITheme
+    {
+        public string TextColor => "black";
+        public string BgrColor => "white";
+    }
+
+    public class DarkTheme : ITheme
+    {
+        public string TextColor => "white";
+        public string BgrColor => "dark gray";
+    }
+
+    public class TrackingThemeFactory
+    {
+        private readonly List<WeakReference<ITheme>> _themes = new();
+
+        public ITheme CreateTheme(bool dark)
+        {
+            ITheme theme = dark ? new DarkTheme() : new LightTheme();
+            _themes.Add(new WeakReference<ITheme>(theme));
+            return theme;
+        }
+
+        public string Info
+        {
+            get
+            {
+                var sb = new StringBuilder();
+
+                foreach (var referense in _themes)
+                {
+                    if (referense.TryGetTarget(out var theme))
+                    {
+                        bool dark = theme is DarkTheme;
+                        sb.Append(dark ? "Dark" : "Light")
+                          .AppendLine(" theme");
+                    }
+                }
+
+                return sb.ToString();
+            }
+        }
+    }
+
+    public class ReplaceableThemeFactry
+    {
+        private readonly List<WeakReference<Ref<ITheme>>> _themes = new();
+
+        private ITheme CreateThemeImpl(bool dark)
+        {
+            return dark ? new DarkTheme() : new LightTheme();
+        }
+
+        public Ref<ITheme> CreateTheme(bool dark)
+        {
+            var r = new Ref<ITheme>(CreateThemeImpl(dark));
+            _themes.Add(new(r));
+            return r;
+        }
+
+        public void ReplaceTheme(bool dark)
+        {
+            foreach (var wr in _themes)
+            {
+                if (wr.TryGetTarget(out var reference))
+                {
+                    reference.Value = CreateThemeImpl(dark);
+                }
+            }
+        }
+    }
+
+    public class Ref<T> where T : class
+    {
+        public T Value;
+
+        public Ref(T value)
+        {
+            Value = value;
+        }
+    }
+
     public static void Run()
     {
         Console.WriteLine("Start -> Factories");
@@ -104,6 +199,21 @@ public static class Factories
 
         var pointFactory = PointFactory.NewCartesianPoint(1.0, Math.PI / 2);
         Console.WriteLine(pointFactory);
+
+        ////////////////////////////////////////
+
+        var factory = new TrackingThemeFactory();
+        var theme1 = factory.CreateTheme(false);
+        var theme2 = factory.CreateTheme(true);
+
+        Console.WriteLine(factory.Info);
+
+        var factory1 = new ReplaceableThemeFactry();
+        var magicTheme = factory1.CreateTheme(true);
+        Console.WriteLine(magicTheme.Value.BgrColor);
+        
+        factory1.ReplaceTheme(false);
+        Console.WriteLine(magicTheme.Value.BgrColor);
 
         Console.WriteLine("Finish -> Factories");
     }
