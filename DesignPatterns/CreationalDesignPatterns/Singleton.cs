5fcafae4fbf525cf9fb0709d23b673925778d3b9
Innovate Core <kolaymaksimiv@gmail.com>
2025-02-11 23:19:26 +0200

    Ambient Context

--- DesignPatterns/CreationalDesignPatterns/Singleton.cs
+++ DesignPatterns/CreationalDesignPatterns/Singleton.cs
@@ -1,6 +1,6 @@
 ﻿using MoreLinq;
 using NUnit.Framework;
-using static DesignPatterns.CreationalDesignPatterns.Singleton;
+using System.Text;
 
 namespace DesignPatterns.CreationalDesignPatterns;
 
@@ -92,7 +92,7 @@ public static class Singleton
             var rf = new ConfigurableRecordFinder(new DummyDatabese());
             var names = new[] { "alpha", "gamma" };
             int tp = rf.GetTotalPopulation(names);
-            
+
             Assert.That(tp, Is.EqualTo(4));
         }
     }
@@ -112,6 +112,99 @@ public static class Singleton
 
         public static PerThreadSingleton Instance => threadInstance.Value;
     }
+
+    //################################################################################################################
+    //##############################################  Ambient Context  ###############################################
+    //################################################################################################################
+
+    // non-thread-safe global context
+    public sealed class BuildingContext : IDisposable
+    {
+        public int WallHeight = 0;
+        public int WallThickness = 300; // etc.
+        private static Stack<BuildingContext> stack
+          = new Stack<BuildingContext>();
+
+        static BuildingContext()
+        {
+            // ensure there's at least one state
+            stack.Push(new BuildingContext(0));
+        }
+
+        public BuildingContext(int wallHeight)
+        {
+            WallHeight = wallHeight;
+            stack.Push(this);
+        }
+
+        public static BuildingContext Current => stack.Peek();
+
+        public void Dispose()
+        {
+            // not strictly necessary
+            if (stack.Count > 1)
+                stack.Pop();
+        }
+    }
+
+    public class Building
+    {
+        public readonly List<Wall> Walls = new List<Wall>();
+
+        public override string ToString()
+        {
+            var sb = new StringBuilder();
+            foreach (var wall in Walls)
+                sb.AppendLine(wall.ToString());
+            return sb.ToString();
+        }
+    }
+
+    public struct Point
+    {
+        private int X, Y;
+
+        public Point(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+
+        public override string ToString()
+        {
+            return $"{nameof(X)}: {X}, {nameof(Y)}: {Y}";
+        }
+    }
+
+    public class Wall
+    {
+        public Point Start, End;
+        public int Height;
+
+        public const int UseAmbient = Int32.MinValue;
+
+        // public Wall(Point start, Point end, int elevation = UseAmbient)
+        // {
+        //   Start = start;
+        //   End = end;
+        //   Elevation = elevation;
+        // }
+
+        public Wall(Point start, Point end)
+        {
+            Start = start;
+            End = end;
+            //Elevation = BuildingContext.Elevation;
+            Height = BuildingContext.Current.WallHeight;
+        }
+
+        public override string ToString()
+        {
+            return $"{nameof(Start)}: {Start}, {nameof(End)}: {End}, " +
+                   $"{nameof(Height)}: {Height}";
+        }
+    }
+
 
     public static void Run()
     {
@@ -133,6 +226,29 @@ public static class Singleton
         });
         Task.WaitAll(t1, t2);
 
+
+        var house = new Building();
+        using (new BuildingContext(3000))
+        {
+            // ground floor
+            //var e = 0;
+            house.Walls.Add(new Wall(new Point(0, 0), new Point(5000, 0)/*, e*/));
+            house.Walls.Add(new Wall(new Point(0, 0), new Point(0, 4000)/*, e*/));
+
+            // first floor
+            //e = 3500;
+            using (new BuildingContext(3500))
+            {
+                house.Walls.Add(new Wall(new Point(0, 0), new Point(5000, 0) /*, e*/));
+                house.Walls.Add(new Wall(new Point(0, 0), new Point(0, 4000) /*, e*/));
+            }
+
+            // back to ground again
+            // e = 0;
+            house.Walls.Add(new Wall(new Point(5000, 0), new Point(5000, 4000)/*, e*/));
+        }
+
+        Console.WriteLine(house);
 
         Console.WriteLine("Finish -> Singleton");
     }
