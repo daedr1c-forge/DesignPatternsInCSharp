2f611419b4812aef1a46367a14feab6c2b7c1048
Innovate Core <kolaymaksimiv@gmail.com>
2025-03-04 23:36:18 +0200

    Memento

---
+++ DesignPatterns/BehavioralDesignPatterns/Memento.cs
@@ -0,0 +1,11 @@
+﻿namespace DesignPatterns.BehavioralDesignPatterns;
+
+public static class Memento
+{
+    public static void Run()
+    {
+        Console.WriteLine("Start -> Memento");
+
+        Console.WriteLine("Finish -> Memento");
+    }
+}
