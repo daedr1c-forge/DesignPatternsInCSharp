4d613dea6c5f447878fcf6d5c90d9787a045d0e2
Daedr1c-Forge <kolaymaksimiv@gmail.com>
2025-03-05 21:01:01 +0200

    Facade

--- DesignPatterns/StructuralDesignPatterns/Facade.cs
+++ DesignPatterns/StructuralDesignPatterns/Facade.cs
@@ -2,9 +2,64 @@
 
 public static class Facade
 {
+    // Subsystem A
+    class SubsystemA
+    {
+        public void OperationA()
+        {
+            Console.WriteLine("SubsystemA: Performing operation A");
+        }
+    }
+
+    // Subsystem B
+    class SubsystemB
+    {
+        public void OperationB()
+        {
+            Console.WriteLine("SubsystemB: Performing operation B");
+        }
+    }
+
+    // Subsystem C
+    class SubsystemC
+    {
+        public void OperationC()
+        {
+            Console.WriteLine("SubsystemC: Performing operation C");
+        }
+    }
+
+    // Facade class to simplify interaction with subsystems
+    public class FacadeSystem
+    {
+        private readonly SubsystemA _subsystemA;
+        private readonly SubsystemB _subsystemB;
+        private readonly SubsystemC _subsystemC;
+
+        public FacadeSystem()
+        {
+            _subsystemA = new SubsystemA();
+            _subsystemB = new SubsystemB();
+            _subsystemC = new SubsystemC();
+        }
+
+        // Simplified method to call multiple subsystems
+        public void Operation()
+        {
+            Console.WriteLine("Facade: Coordinating subsystems...");
+            _subsystemA.OperationA();
+            _subsystemB.OperationB();
+            _subsystemC.OperationC();
+            Console.WriteLine("Facade: Operation completed");
+        }
+    }
+
     public static void Run()
     {
         Console.WriteLine("Start -> Facade");
+
+        FacadeSystem facade = new FacadeSystem();
+        facade.Operation();
 
         Console.WriteLine("Finish -> Facade");
     }
